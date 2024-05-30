dea79426b90b91201de0aedaf32e7ec9f81e45e4
AKP007TRUWORTH <anil.prajapat@truworthwellness.com>
2024-05-30 18:43:10 +0530

    optimzie code

--- app.config.js
+++ app.config.js
@@ -1,46 +1,23 @@
 require('dotenv').config();
 
 module.exports = ({ config }) => {
-    if (process.env.CODE_PUSH_ENIVIRONMENT === 'development') {
-        return {
-            ...config,
-            "extra": {
-                "eas": {
-                    "projectId": "12db7102-c890-41d6-9c32-7b03482a524c"
-                }
-            },
-            "updates": {
-                "url": "https://u.expo.dev/12db7102-c890-41d6-9c32-7b03482a524c",
-                "requestHeaders": {
-                    "expo-runtime-version": "1.0.0",
-                    "expo-channel-name": "development"
-                },
-                "platforms": ["ios", "android"]
-            },
-            "ios": {
-                "bundleIdentifier": "com.akp007.awesomeproject",
+    return {
+        ...config,
+        "extra": {
+            "eas": {
+                "projectId": "12db7102-c890-41d6-9c32-7b03482a524c"
             }
-        }
-    }
-    if (process.env.CODE_PUSH_ENIVIRONMENT === 'production') {
-        return {
-            ...config,
-            "extra": {
-                "eas": {
-                    "projectId": "12db7102-c890-41d6-9c32-7b03482a524c"
-                }
+        },
+        "updates": {
+            "url": "https://u.expo.dev/12db7102-c890-41d6-9c32-7b03482a524c",
+            "requestHeaders": {
+                "expo-runtime-version": "2.0.0",
+                "expo-channel-name": process.env.CODE_PUSH_ENIVIRONMENT
             },
-            "updates": {
-                "url": "https://u.expo.dev/12db7102-c890-41d6-9c32-7b03482a524c",
-                "requestHeaders": {
-                    "expo-runtime-version": "1.0.0",
-                    "expo-channel-name": "production"
-                },
-                "platforms": ["ios", "android"]
-            },
-            "ios": {
-                "bundleIdentifier": "com.akp007.awesomeproject",
-            }
+            "platforms": ["ios", "android"]
+        },
+        "ios": {
+            "bundleIdentifier": "com.akp007.awesomeproject",
         }
     }
 }
